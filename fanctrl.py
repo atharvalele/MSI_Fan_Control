e84c380017295b7d7d650315a92d790edcfb8abc
Atharva Lele <itsatharva@gmail.com>
2018-04-02 00:00:54 +0530

    Add ability to write registers

--- fanctrl.py
+++ fanctrl.py
@@ -13,5 +13,22 @@ if not os.path.exists(EC_FILE):
         os.system('modprobe ec_sys write_support=1')
 
 # open EC file in binary mode
-f = open(EC_FILE, 'rb')
-print(f.readlines())
+f = open(EC_FILE, 'r+b')
+
+# open profile file
+pf = open(PROFILE_FILE, 'r')
+
+def write_ec(value, address):
+        f.seek(address)
+        # read in old value byte, ord() used so that python outputs unicode value
+        old_val = ord(f.read(1))
+        if old_val != value:
+                print('{0}: {1} -> {2}'.format(address, old_val, value))
+                f.write(bytearray([value]))
+        else:
+                print('{0}: {1}'.format(address, value))
+
+for line in pf.readlines():
+        if line.startswith('>'):
+                address, val = line.split()[1:3]
+                write_ec(int(val, 16), int(address,16))
